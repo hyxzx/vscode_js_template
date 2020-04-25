920e31383cd238c4cf4793b439be29198100227e
kkone <kk@kk.com>
2020-04-25 14:44:54 +0800

    配置rollup打包

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs'
+
+export default {
+    input: "src/index.js",
+    output: {
+        file: 'lib/index.js',
+        format: 'iife',
+        sourcemap: true
+    },
+    plugins: [
+        resolve({
+            browser: true
+        }),
+        commonjs(),
+        babel({
+            exclude: 'node_modules/**'
+        }),
+    ]
+}
